4e6b6c75e0720f852f28dbe1ce615aa8a80efbaf
mattybojo <mattbojorquez@gmail.com>
2022-06-12 15:26:07 -0700

    Show all digits of last 4 of gift cards

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SortOrderCaretComponent } from './sort-order-caret/sort-order-caret.component';
+import { NoCommaPipe } from './no-comma.pipe';
 
 @NgModule({
-  declarations: [SortOrderCaretComponent],
+  declarations: [SortOrderCaretComponent, NoCommaPipe],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [SortOrderCaretComponent]
+  exports: [SortOrderCaretComponent, NoCommaPipe]
 })
 export class SharedModule { }
