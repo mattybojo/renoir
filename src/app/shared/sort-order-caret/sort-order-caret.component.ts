820852385ef3827a837d0374b89955641fd2f7b5
mattybojo <mattbojorquez@gmail.com>
2022-06-13 21:47:26 -0700

    Removed unnecessary OnInit in SortOrderCaret

--- src/app/shared/sort-order-caret/sort-order-caret.component.ts
+++ src/app/shared/sort-order-caret/sort-order-caret.component.ts
@@ -1,16 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ren-sort-order-caret',
   templateUrl: './sort-order-caret.component.html',
   styleUrls: ['./sort-order-caret.component.scss'],
 })
-export class SortOrderCaretComponent implements OnInit {
+export class SortOrderCaretComponent {
   @Input() param: string;
   @Input() sortOrder: string;
   @Input() sortParam: string;
-
-  ngOnInit(): void {
-    console.log();
-  }
 }
