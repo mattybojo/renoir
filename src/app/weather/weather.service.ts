a92f5e4351faa8ce1d8cf0fe1f697466d8af6125
mattybojo <mattbojorquez@gmail.com>
2022-07-06 21:59:24 -0700

    Added debug switch to weather service to reduce API usage during development

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -15,8 +15,55 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherData(): void {
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
-    this.http.get<Weather>(weatherUrl).subscribe(weatherData => this.setWeatherObs(weatherData));
+    const debugMode = false;
+    if (debugMode) {
+      this.setWeatherObs({
+        coord: {
+          lon: -121.3238,
+          lat: 38.0563
+        },
+        weather: [
+          {
+            id: 800,
+            main: 'Clear',
+            description: 'clear sky',
+            icon: '01d'
+          }
+        ],
+        base: 'stations',
+        main: {
+          temp: 69.66,
+          feels_like: 68.77,
+          temp_min: 67.08,
+          temp_max: 70.32,
+          pressure: 1015,
+          humidity: 52
+        },
+        visibility: 10000,
+        wind: {
+          speed: 8.05,
+          deg: 300
+        },
+        clouds: {
+          all: 0
+        },
+        dt: 1656873783,
+        sys: {
+          type: 1,
+          id: 5789,
+          country: 'US',
+          sunrise: 1656852428,
+          sunset: 1656905499
+        },
+        timezone: -25200,
+        id: 5366524,
+        name: 'Lincoln Village',
+        cod: 200
+      });
+    } else {
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+      this.http.get<Weather>(weatherUrl).subscribe(weatherData => this.setWeatherObs(weatherData));
+    }
   };
 
   getWeatherObs(): Observable<Weather> {
