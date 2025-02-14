41f9e68a167c177f68d5f8466efca3a0f3e759e4
mattybojo <mattbojorquez@gmail.com>
2025-02-14 14:55:31 -0800

    Added font-awesome and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,76 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: {
+        default: "var(--ion-color-primary)",
+        shade: "var(--ion-color-primary-shade)",
+        tint: "var(--ion-color-primary-tint)",
+      },
+      secondary: {
+        default: "var(--ion-color-secondary)",
+        shade: "var(--ion-color-secondary-shade)",
+        tint: "var(--ion-color-secondary-tint)",
+      },
+      tertiary: {
+        default: "var(--ion-color-tertiary)",
+        shade: "var(--ion-color-tertiary-shade)",
+        tint: "var(--ion-color-tertiary-tint)",
+      },
+      success: {
+        default: "var(--ion-color-success)",
+        shade: "var(--ion-color-tertiary-success)",
+        tint: "var(--ion-color-tertiary-success)",
+      },
+      warning: {
+        default: "var(--ion-color-warning)",
+        shade: "var(--ion-color-tertiary-warning)",
+        tint: "var(--ion-color-tertiary-warning)",
+      },
+      danger: {
+        default: "var(--ion-color-danger)",
+        shade: "var(--ion-color-tertiary-danger)",
+        tint: "var(--ion-color-tertiary-danger)",
+      },
+      dark: {
+        default: "var(--ion-color-dark)",
+        shade: "var(--ion-color-tertiary-dark)",
+        tint: "var(--ion-color-tertiary-dark)",
+      },
+      medium: {
+        default: "var(--ion-color-medium)",
+        shade: "var(--ion-color-tertiary-medium)",
+        tint: "var(--ion-color-tertiary-medium)",
+      },
+      light: {
+        default: "var(--ion-color-light)",
+        shade: "var(--ion-color-tertiary-light)",
+        tint: "var(--ion-color-tertiary-light)",
+      },
+      step50: "var(--ion-color-step-50)",
+      step100: "var(--ion-color-step-100)",
+      step150: "var(--ion-color-step-150)",
+      step200: "var(--ion-color-step-200)",
+      step250: "var(--ion-color-step-250)",
+      step300: "var(--ion-color-step-300)",
+      step350: "var(--ion-color-step-350)",
+      step400: "var(--ion-color-step-400)",
+      step450: "var(--ion-color-step-450)",
+      step500: "var(--ion-color-step-500)",
+      step550: "var(--ion-color-step-550)",
+      step600: "var(--ion-color-step-600)",
+      step650: "var(--ion-color-step-650)",
+      step700: "var(--ion-color-step-700)",
+      step750: "var(--ion-color-step-750)",
+      step800: "var(--ion-color-step-800)",
+      step850: "var(--ion-color-step-850)",
+      step900: "var(--ion-color-step-900)",
+      step950: "var(--ion-color-step-950)",
+      white: "#fff",
+      black: "#000",
+    },
+  },
+  plugins: [],
+};
